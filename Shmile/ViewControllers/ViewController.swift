a9887e400c04ed9ed59204e0d4ef8192db1571c2
Glen Wong <glen@xoocorp.com>
2015-06-27 15:18:34 -0700

    set up first button

--- Shmile/ViewControllers/ViewController.swift
+++ Shmile/ViewControllers/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  var buttonView:UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    let size = self.view.frame.size
+    buttonView = UIButton(frame: CGRectMake((size.width - 300) / 2, (size.height - 100) / 2, 300, 100))
+    buttonView.backgroundColor = UIColor.blueColor()
+    buttonView.layer.cornerRadius = 25
+    buttonView.layer.borderWidth = 2
+    buttonView.setTitle("Begin", forState: UIControlState.Normal)
+    buttonView.addTarget(self, action: "onBeginClicked", forControlEvents: UIControlEvents.TouchUpInside)
+    self.view.addSubview(buttonView)
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +30,8 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  func onBeginClicked() {
+    NSLog("Begin clicked!")
+  }
 }
 
