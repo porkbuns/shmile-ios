6e6d2adcaa3506ef58195af0fba7580b3cd3dd76
Glen Wong <glen@xoocorp.com>
2015-06-27 14:52:26 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+use_frameworks!
+
+target 'Shmile' do
+  pod "DKChainableAnimationKit", "~> 1.5.0"
+  pod 'Socket.IO-Client-Swift', '~> 2.3'
+end
+
+target 'ShmileTests' do
+
+end
---
+++ Pods/Target Support Files/Pods-Shmile/Pods-Shmile-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DKChainableAnimationKit
+#define COCOAPODS_POD_AVAILABLE_DKChainableAnimationKit
+#define COCOAPODS_VERSION_MAJOR_DKChainableAnimationKit 1
+#define COCOAPODS_VERSION_MINOR_DKChainableAnimationKit 5
+#define COCOAPODS_VERSION_PATCH_DKChainableAnimationKit 0
+
+// Socket.IO-Client-Swift
+#define COCOAPODS_POD_AVAILABLE_Socket_IO_Client_Swift
+#define COCOAPODS_VERSION_MAJOR_Socket_IO_Client_Swift 2
+#define COCOAPODS_VERSION_MINOR_Socket_IO_Client_Swift 3
+#define COCOAPODS_VERSION_PATCH_Socket_IO_Client_Swift 6
+
